e637e5b55d2b2413e8360644502e064ad6167125
Erçetin Güler <ercetinguler@hotmail.com>
2022-08-19 14:16:30 +0300

    Initialize Codes-Exe-patch-1

---
+++ src/aws.ts
@@ -0,0 +1,85 @@
+import * as semver from 'semver';
+import * as exec from '@actions/exec';
+import * as io from '@actions/io';
+
+const ecrRegistryRegex = /^(([0-9]{12})\.dkr\.ecr\.(.+)\.amazonaws\.com(.cn)?)(\/([^:]+)(:.+)?)?$/;
+
+export const isECR = (registry: string): boolean => {
+  return ecrRegistryRegex.test(registry) || isPubECR(registry);
+};
+
+export const isPubECR = (registry: string): boolean => {
+  return registry === 'public.ecr.aws';
+};
+
+export const getRegion = (registry: string): string => {
+  if (isPubECR(registry)) {
+    return process.env.AWS_REGION || process.env.AWS_DEFAULT_REGION || 'us-east-1';
+  }
+  const matches = registry.match(ecrRegistryRegex);
+  if (!matches) {
+    return '';
+  }
+  return matches[3];
+};
+
+export const getAccountIDs = (registry: string): string[] => {
+  if (isPubECR(registry)) {
+    return [];
+  }
+  const matches = registry.match(ecrRegistryRegex);
+  if (!matches) {
+    return [];
+  }
+  let accountIDs: Array<string> = [matches[2]];
+  if (process.env.AWS_ACCOUNT_IDS) {
+    accountIDs.push(...process.env.AWS_ACCOUNT_IDS.split(','));
+  }
+  return accountIDs.filter((item, index) => accountIDs.indexOf(item) === index);
+};
+
+export const getCLI = async (): Promise<string> => {
+  return io.which('aws', true);
+};
+
+export const execCLI = async (args: string[]): Promise<string> => {
+  return exec
+    .getExecOutput(await getCLI(), args, {
+      ignoreReturnCode: true,
+      silent: true
+    })
+    .then(res => {
+      if (res.stderr.length > 0 && res.exitCode != 0) {
+        throw new Error(res.stderr.trim());
+      } else if (res.stderr.length > 0) {
+        return res.stderr.trim();
+      } else {
+        return res.stdout.trim();
+      }
+    });
+};
+
+export const getCLIVersion = async (): Promise<string> => {
+  return parseCLIVersion(await execCLI(['--version']));
+};
+
+export const parseCLIVersion = async (stdout: string): Promise<string> => {
+  const matches = /aws-cli\/([0-9.]+)/.exec(stdout);
+  if (!matches) {
+    throw new Error(`Cannot parse AWS CLI version`);
+  }
+  return semver.clean(matches[1]);
+};
+
+export const getDockerLoginCmds = async (cliVersion: string, registry: string, region: string, accountIDs: string[]): Promise<string[]> => {
+  let ecrCmd = (await isPubECR(registry)) ? 'ecr-public' : 'ecr';
+  if (semver.satisfies(cliVersion, '>=2.0.0') || (await isPubECR(registry))) {
+    return execCLI([ecrCmd, 'get-login-password', '--region', region]).then(pwd => {
+      return [`docker login --username AWS --password ${pwd} ${registry}`];
+    });
+  } else {
+    return execCLI([ecrCmd, 'get-login', '--region', region, '--registry-ids', accountIDs.join(' '), '--no-include-email']).then(dockerLoginCmds => {
+      return dockerLoginCmds.trim().split(`\n`);
+    });
+  }
+};
---
+++ src/docker.ts
@@ -0,0 +1,89 @@
+import * as aws from './aws';
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+
+export async function login(registry: string, username: string, password: string): Promise<void> {
+  if (await aws.isECR(registry)) {
+    await loginECR(registry, username, password);
+  } else {
+    await loginStandard(registry, username, password);
+  }
+}
+
+export async function logout(registry: string): Promise<void> {
+  await exec
+    .getExecOutput('docker', ['logout', registry], {
+      ignoreReturnCode: true
+    })
+    .then(res => {
+      if (res.stderr.length > 0 && res.exitCode != 0) {
+        core.warning(res.stderr.trim());
+      }
+    });
+}
+
+export async function loginStandard(registry: string, username: string, password: string): Promise<void> {
+  if (!username || !password) {
+    throw new Error('Username and password required');
+  }
+
+  let loginArgs: Array<string> = ['login', '--password-stdin'];
+  loginArgs.push('--username', username);
+  loginArgs.push(registry);
+
+  if (registry) {
+    core.info(`Logging into ${registry}...`);
+  } else {
+    core.info(`Logging into Docker Hub...`);
+  }
+  await exec
+    .getExecOutput('docker', loginArgs, {
+      ignoreReturnCode: true,
+      silent: true,
+      input: Buffer.from(password)
+    })
+    .then(res => {
+      if (res.stderr.length > 0 && res.exitCode != 0) {
+        throw new Error(res.stderr.trim());
+      }
+      core.info(`Login Succeeded!`);
+    });
+}
+
+export async function loginECR(registry: string, username: string, password: string): Promise<void> {
+  const cliPath = await aws.getCLI();
+  const cliVersion = await aws.getCLIVersion();
+  const region = await aws.getRegion(registry);
+  const accountIDs = await aws.getAccountIDs(registry);
+
+  if (await aws.isPubECR(registry)) {
+    core.info(`AWS Public ECR detected with ${region} region`);
+  } else {
+    core.info(`AWS ECR detected with ${region} region`);
+  }
+
+  process.env.AWS_ACCESS_KEY_ID = username || process.env.AWS_ACCESS_KEY_ID;
+  process.env.AWS_SECRET_ACCESS_KEY = password || process.env.AWS_SECRET_ACCESS_KEY;
+
+  core.info(`Retrieving docker login command through AWS CLI ${cliVersion} (${cliPath})...`);
+  const loginCmds = await aws.getDockerLoginCmds(cliVersion, registry, region, accountIDs);
+
+  core.info(`Logging into ${registry}...`);
+  loginCmds.forEach((loginCmd, index) => {
+    exec
+      .getExecOutput(loginCmd, [], {
+        ignoreReturnCode: true,
+        silent: true
+      })
+      .then(res => {
+        if (res.stderr.length > 0 && res.exitCode != 0) {
+          throw new Error(res.stderr.trim());
+        }
+        if (loginCmds.length > 1) {
+          core.info(`Login Succeeded! (${index}/${loginCmds.length})`);
+        } else {
+          core.info('Login Succeeded!');
+        }
+      });
+  });
+}
